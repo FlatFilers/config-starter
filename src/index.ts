9bdb860960556bcb7eaa05f7f56cd34c58f2fdda
Alnoor Pirani <alnoorp@gmail.com>
2024-03-08 23:29:35 +0000

    Update tsconfig.json and package.json

--- src/index.ts
+++ src/index.ts
@@ -48,28 +48,7 @@ export default function flatfileEventListener(listener: Client) {
                   label: "Email",
                 },
               ],
-            },
-            {
-              name: "Sheet 2",
-              slug: "sheet2",
-              fields: [
-                {
-                  key: "firstName",
-                  type: "string",
-                  label: "First Name",
-                },
-                {
-                  key: "lastName",
-                  type: "string",
-                  label: "Last Name",
-                },
-                {
-                  key: "email",
-                  type: "string",
-                  label: "Email",
-                },
-              ],
-            },
+            }
           ],
           actions: [
             {
